0ff97e7b74185eb6c60f56ac33033d5c94af9aa3
MaryamAbedinnejad <Maryam.Abedinnejad1@gmail.com>
2025-11-07 21:17:34 -0500

    Fix capitalization and directory handling for autograder

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -29,14 +29,14 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-Mkdir data #create data directory
+mkdir data #create data directory
 
 # 2. Move the ./rawdata directory to ./data/raw
-Mv rawdata data/raw 
+mv rawdata data/raw 
 # 3. List the contents of the ./data/raw directory
-Ls data/raw
+ls data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-Mkdir -p data/processed/server_logs data/processed/user_logs data/processed/event_logs
+mkdir -p data/processed/server_logs data/processed/user_logs data/processed/event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cp data/raw/*server*.log data/processed/server_logs/
 # 6. Repeat the above step for user logs and event logs
